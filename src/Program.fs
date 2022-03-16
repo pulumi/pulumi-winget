956ecdc9f020f2cff4e860aeace537f06dbe2138
Zaid Ajaj <zaid.naom@gmail.com>
2022-03-16 23:54:18 +0100

    Use only media template instead of media

--- src/Program.fs
+++ src/Program.fs
@@ -120,11 +120,13 @@ let generateMsi() =
                         Wix.attr "Compressed" "yes"
                     ]
 
-                    Wix.media [ 
-                        Wix.attr "Id" "1"
-                        Wix.attr "Cabinet" "1"
-                        Wix.attr "EmbedCab" "yes"
-                    ]
+                    // Wix.media [ 
+                    //    Wix.attr "Id" "1"
+                    //    Wix.attr "Cabinet" "1"
+                    //    Wix.attr "EmbedCab" "yes"
+                    // ]
+
+                    Wix.mediaTemplate [ Wix.attr "EmbedCab" "yes" ]
 
                     Wix.directory "TARGETDIR" "SourceDir" [
                         Wix.directoryId "ProgramFilesFolder" [
